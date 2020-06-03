c54d3a64eed5afb18d18d45d6a39cb5b98cda6b8
EvanGee <evangiese77@gmail.com>
2020-06-03 09:14:40 -0600

    remove  undeed experiment stdint

--- Program/include/_stdint.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef _BITS_STDINT_UINTN_H
-#define _BITS_STDINT_UINTN_H	1
-
-typedef unsigned char uint8_t;
-typedef unsigned short int uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
-#endif
