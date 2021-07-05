a6f68a089d29520ad5c4b69897668eabe3b0235c
evangee <evangiese77@gmail.com>
2021-07-05 11:59:00 -0600

    remove libcsp

--- Program/libcsp/build/c4che/build.config.py
+++
@@ -1,2 +0,0 @@
-version = 0x2001200
-tools = [{'tool': 'ar', 'tooldir': None, 'funs': None}, {'tool': 'c', 'tooldir': None, 'funs': None}, {'tool': 'gcc', 'tooldir': None, 'funs': None}, {'tool': 'compiler_c', 'tooldir': None, 'funs': None}]
